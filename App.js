09956fccf06560566b2f2f810a61caeb1a4a2d57
Eugeny Semenov <et@takeseoeasy.com>
2020-09-08 14:21:23 +0700

    add Ioicons and SectionTitle

--- App.js
+++ App.js
@@ -1,74 +1,130 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SectionList } from 'react-native';
 import styled from 'styled-components/native';
+import { Ionicons } from '@expo/vector-icons';
+
+import { Appointment, SectionTitle } from "./components";
+
+const DATA = [
+    {
+        title: "10 сентября",
+        data: [
+            {
+                time: '15:30',
+                diagnosis: 'пульпит',
+                active: true,
+                user: {
+                    fullname: 'Лира Тычинина',
+                    avatar: 'https://sun9-50.userapi.com/impg/jaZ5Ua8cucsSTvCC03rQ7EsEeW4vrPoULNGbyA/4Fgo-y1m6tE.jpg?size=100x0&quality=88&crop=0,189,1620,1620&sign=3dc05f0cf824f47431308cd89b3f3634&ava=1',
+                }
+            },
+            {
+                time: '19:30',
+                diagnosis: 'вставная челюсть',
+                user: {
+                    fullname: 'Денис Тычинин',
+                    avatar: 'https://sun9-44.userapi.com/impf/rg7aBwI3cCLSAKnFPi11rJ2NXdLfPih6jggIUQ/b_fAGz160iQ.jpg?size=200x0&quality=90&crop=0,0,1333,1333&sign=213d37a2d691a67fa3249e2bd2435f52&ava=1',
+                }
+            },
+            {
+                time: '15:30',
+                diagnosis: 'пульпит',
+                active: true,
+                user: {
+                    fullname: 'Денис Тычинин',
+                    avatar: 'https://sun9-50.userapi.com/impg/jaZ5Ua8cucsSTvCC03rQ7EsEeW4vrPoULNGbyA/4Fgo-y1m6tE.jpg?size=100x0&quality=88&crop=0,189,1620,1620&sign=3dc05f0cf824f47431308cd89b3f3634&ava=1',
+                }
+            },
+            {
+                time: '19:30',
+                diagnosis: 'вставная челюсть',
+                user: {
+                    fullname: 'Денис Тычинин',
+                    avatar: 'https://sun9-44.userapi.com/impf/rg7aBwI3cCLSAKnFPi11rJ2NXdLfPih6jggIUQ/b_fAGz160iQ.jpg?size=200x0&quality=90&crop=0,0,1333,1333&sign=213d37a2d691a67fa3249e2bd2435f52&ava=1',
+                }
+            }
+        ],
+    },
+    {
+        title: "16 сентября",
+        data: [
+            {
+                time: '15:30',
+                diagnosis: 'пульпит',
+                user: {
+                    fullname: 'Денис Тычинин',
+                    avatar: 'https://sun9-50.userapi.com/impg/jaZ5Ua8cucsSTvCC03rQ7EsEeW4vrPoULNGbyA/4Fgo-y1m6tE.jpg?size=100x0&quality=88&crop=0,189,1620,1620&sign=3dc05f0cf824f47431308cd89b3f3634&ava=1',
+                }
+            },
+            {
+                time: '19:30',
+                diagnosis: 'вставная челюсть',
+                user: {
+                    fullname: 'Денис Тычинин',
+                    avatar: 'https://sun9-44.userapi.com/impf/rg7aBwI3cCLSAKnFPi11rJ2NXdLfPih6jggIUQ/b_fAGz160iQ.jpg?size=200x0&quality=90&crop=0,0,1333,1333&sign=213d37a2d691a67fa3249e2bd2435f52&ava=1',
+                }
+            },
+            {
+                time: '15:30',
+                diagnosis: 'пульпит',
+                user: {
+                    fullname: 'Денис Тычинин',
+                    avatar: 'https://sun9-50.userapi.com/impg/jaZ5Ua8cucsSTvCC03rQ7EsEeW4vrPoULNGbyA/4Fgo-y1m6tE.jpg?size=100x0&quality=88&crop=0,189,1620,1620&sign=3dc05f0cf824f47431308cd89b3f3634&ava=1',
+                }
+            },
+            {
+                time: '19:30',
+                diagnosis: 'вставная челюсть',
+                user: {
+                    fullname: 'Денис Тычинин',
+                    avatar: 'https://sun9-44.userapi.com/impf/rg7aBwI3cCLSAKnFPi11rJ2NXdLfPih6jggIUQ/b_fAGz160iQ.jpg?size=200x0&quality=90&crop=0,0,1333,1333&sign=213d37a2d691a67fa3249e2bd2435f52&ava=1',
+                }
+            }
+        ]
+    },
+];
 
 export default function App() {
   return (
     <Container>
-      <Group>
-        <GroupTitle>11 сентября</GroupTitle>
-        <GroupItem>
-          <Avatar source={{
-            uri: 'https://sun9-44.userapi.com/impf/rg7aBwI3cCLSAKnFPi11rJ2NXdLfPih6jggIUQ/b_fAGz160iQ.jpg?size=200x0&quality=90&crop=0,0,1333,1333&sign=213d37a2d691a67fa3249e2bd2435f52&ava=1'
-          }} />
-          <View style={{ flex: 1 }}>
-            <FullName>Марина Алейникова</FullName>
-            <GrayText>пульпит, удаление зуба</GrayText>
-          </View>
-          <GroupDate>12:30</GroupDate>
-        </GroupItem>
-      </Group>
+        <SectionList
+            sections={DATA}
+            keyExtractor={(item, index) => index}
+            renderItem={({ item }) => <Appointment {...item} />}
+            renderSectionHeader={({ section: { title } }) => (
+                <SectionTitle>{title}</SectionTitle>
+            )}
+        />
+        <PlusButton style={
+            {shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: 0.8,
+            shadowRadius: 2,
+            elevation: 6,}
+        }
+        >
+            <Ionicons name="ios-add" size={36} color="#fff" />
+        </PlusButton>
     </Container>
   );
 }
 
-const GroupDate = styled.Text`
-   border-radius: 18px;
-   background: #e9f5ff;
-   font-weight: 600;
-   color: #4294ff;
-   font-size: 14px;
-   width: 70px;
-   text-align: center;
-   display: flex;
-   align-items: center;
-   padding: 1%;
-`;
-
-const GrayText = styled.Text`
- font-size: 16px;
- color: #8b979f;
-`;
-
-const FullName = styled.Text`
-  font-size: 16px;
-  font-weight: 600;
-`;
-
-const Avatar = styled.Image`
+const PlusButton = styled.TouchableOpacity`
+align-items: center;
+justify-content: center;
 border-radius: 50px;
-width: 60px;
-height: 60px;
-margin-right: 15px;
-`;
-
-const GroupItem = styled.View`
-  padding: 20px 0;
-  flex-direction: row;
-  align-items: center;
-`;
-
-const GroupTitle = styled.Text`
-   font-weight: 800;
-   font-size: 22px;
-   color: #000;
-`;
-
-const Group = styled.View`
-    padding: 0 20px;
+width: 64px;
+height: 64px;
+background: #2a86ff;
+position: absolute;
+right: 15px;
+bottom: 15px;
 `;
 
 const Container = styled.View`
-  flex: 1;
-margin-top: 50px;
+ flex: 1;
+margin-top: 30px;
 `;
---
+++ components/Appointment.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+const Appointment = ({ user, diagnosis, active, time }) => {
+    return (
+                <GroupItem>
+                    <Avatar source={{
+                        uri:
+                        user.avatar
+                    }} />
+                    <View style={{ flex: 1 }}>
+                        <FullName>{user.fullname}</FullName>
+                        <GrayText>{diagnosis}</GrayText>
+                    </View>
+                    <GroupDate active={active}>{time}</GroupDate>
+                </GroupItem>
+    );
+}
+
+Appointment.defaultProps = {
+    groupTitle:'untitled',
+     items: [],
+}
+
+const GroupDate = styled.Text`
+   background: ${props => (props.active ? '#2a86ff' : '#e9f5ff')};
+   color: ${props => (props.active ? '#fff' : '#4294ff')};
+   border-radius: 18px;
+   font-weight: 600;
+   font-size: 14px;
+   width: 70px;
+   text-align: center;
+   display: flex;
+   align-items: center;
+   padding: 1%;
+`;
+
+const GrayText = styled.Text`
+ font-size: 16px;
+ color: #8b979f;
+`;
+
+const FullName = styled.Text`
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+const Avatar = styled.Image`
+border-radius: 50px;
+width: 60px;
+height: 60px;
+margin-right: 15px;
+`;
+
+const GroupItem = styled.TouchableOpacity`
+  padding: 20px;
+  flex-direction: row;
+  align-items: center;
+  borderBottomWidth: 1px;
+  borderBottomColor: #f3f3f3;
+`;
+
+export default Appointment;
